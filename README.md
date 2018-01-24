5e79aae5dc01e06c74698af7c423a4a6e663adb3
ilovehci <ilovehci@users.noreply.github.com>
2018-01-24 21:16:11 +0800

    putting up android project

---
+++ README.md
@@ -0,0 +1,2 @@
+# ict2105-lab01-2018
+A simple app project
---
+++ app/src/main/java/course/lab/firstapp/MainActivity.java
@@ -0,0 +1,19 @@
+package course.lab.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO: Add your logging message here.
+
+        Log.d(TAG, "Attack of the killer androids");
+
+    }
+}
